5b4bab578535af7c22a7a85607506f7e04e5b4eb
haram22 <kimhr9889@handong.ac.kr>
2024-07-06 20:04:43 +0900

    [Feat] Qr server 연결 성공 !

--- PARD/Sources/API/QrController.swift
+++ PARD/Sources/API/QrController.swift
@@ -5,43 +5,22 @@
 //  Created by 김하람 on 7/3/24.
 //
 
-import Foundation
-
-func getValidQR1(qrUrl : String) {
-    if let urlLink = URL(string: url + "/validQR") {
-        let session = URLSession(configuration: .default)
-        let task = session.dataTask(with: urlLink) { data, response, error in
-            if let error = error {
-                print("🚨 Error:", error)
-                return
-            }
-            guard let JSONdata = data, !JSONdata.isEmpty else {
-                print("🚨 Error: No data or empty data")
-                return
-            }
-            
-            // 응답 데이터를 문자열로 변환하여 출력
-            if let dataString = String(data: JSONdata, encoding: .utf8) {
-                print("Response Data String: \(dataString)")
-            } else {
-                print("🚨 Error: Unable to convert data to string")
-            }
-        }
-        task.resume()
-    }
-}
+import UIKit
 
 func getValidQR(with qrUrl: String) {
+    print("❤️ \(qrUrl)")
     guard let url = URL(string: "\(url)/validQR") else {
         print("🚨 Invalid URL")
         return
     }
+
     var request = URLRequest(url: url)
-    request.httpMethod = "GET"
+    request.httpMethod = "POST"
     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
     
     let body: [String: AnyHashable] = [
-        "qrurl": qrUrl
+        "qrUrl": qrUrl,
+        "seminar": "part"
     ]
     request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
     
@@ -50,6 +29,7 @@ func getValidQR(with qrUrl: String) {
             print("🚨 Error: \(error?.localizedDescription ?? "Unknown error")")
             return
         }
+
         if let responseString = String(data: data, encoding: .utf8) {
             print("✅ success: \(responseString)")
         } else {
