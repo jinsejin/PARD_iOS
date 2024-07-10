5594007248673baefded883f7ce52ab730335b25
mingseobi <22200078@handong.ac.kr>
2024-07-10 16:54:24 +0900

    [Feat] git pull

--- PARD/Sources/MyScore/ViewControllers/MyScoreViewController.swift
+++ PARD/Sources/MyScore/ViewControllers/MyScoreViewController.swift
@@ -127,8 +127,6 @@ class MyScoreViewController: UIViewController {
         }
     }
     
-    
-    
     private func setupRankingButton() {
         let rankingButton = UIButton(type: .system).then {
             $0.setTitle("전체랭킹 확인하기", for: .normal)
