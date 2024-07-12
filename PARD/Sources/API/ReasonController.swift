315bd4392c7ce3b7af85b1d4ac7a406dcf4e5c47
mingseobi <22200078@handong.ac.kr>
2024-07-12 18:29:27 +0900

    scorepolicy tooltip Update

--- PARD/Sources/API/ReasonController.swift
+++ PARD/Sources/API/ReasonController.swift
@@ -7,35 +7,40 @@
 
 import UIKit
 
-func getReason(completion: @escaping ([Reason]?) -> Void) {
-    guard let urlLink = URL(string: url + "/reason") else {
-        print("Invalid URL")
-        completion(nil)
-        return
+func getReason() {
+    if let urlLink = URL(string: url + "/reason") {
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: urlLink) { data, response, error in
+            if let error = error {
+                print("🚨 Error:", error)
+                return
+            }
+            guard let JSONdata = data, !JSONdata.isEmpty else {
+                print("🚨 [getReason] Error: No data or empty data")
+                return
+            }
+            
+            // 응답 데이터를 문자열로 변환하여 출력
+            if let dataString = String(data: JSONdata, encoding: .utf8) {
+                print("🌱 Response Data String: \(dataString)")
+            } else {
+                print("🚨🚨 Error: Unable to convert data to string")
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                // 먼저 JSON 데이터를 단일 객체로 디코딩 시도
+                if let reason = try? decoder.decode(Reason.self, from: JSONdata) {
+                    print("✅ Success: \(reason)")
+                } else {
+                    // 단일 객체로 디코딩이 실패하면 배열로 디코딩 시도
+                    let reasonArray = try decoder.decode([Reason].self, from: JSONdata)
+                    print("✅ Success: \(reasonArray)")
+                }
+            } catch {
+                print("🚨 Decoding Error:", error)
+            }
+        }
+        task.resume()
     }
-
-    let session = URLSession(configuration: .default)
-    let task = session.dataTask(with: urlLink) { data, response, error in
-        if let error = error {
-            print("🚨 Error:", error)
-            completion(nil)
-            return
-        }
-        guard let JSONdata = data, !JSONdata.isEmpty else {
-            print("🚨 [getReason] Error: No data or empty data")
-            completion(nil)
-            return
-        }
-
-        let decoder = JSONDecoder()
-        do {
-            let reasons = try decoder.decode([Reason].self, from: JSONdata)
-            print("✅ Success: Fetched Reasons - \(reasons)")
-            completion(reasons)
-        } catch {
-            print("🚨 Decoding Error:", error)
-            completion(nil)
-        }
-    }
-    task.resume()
 }
