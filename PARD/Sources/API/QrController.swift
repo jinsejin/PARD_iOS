b3499c055ec0d7677143a2dc34e27db73e2b9b0d
haram22 <kimhr9889@handong.ac.kr>
2024-07-20 20:56:48 +0900

    [Feat] 유효하지 않은 큐알 팝업 연결 및 팝업 생성

--- PARD/Sources/API/QrController.swift
+++ PARD/Sources/API/QrController.swift
@@ -6,35 +6,48 @@
 //
 
 import UIKit
-
-func getValidQR(with qrUrl: String) {
-    print("❤️ \(qrUrl)")
-    guard let url = URL(string: "\(url)/validQR") else {
-        print("🚨 Invalid URL")
-        return
-    }
-
-    var request = URLRequest(url: url)
-    request.httpMethod = "POST"
-    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-    
-    let body: [String: AnyHashable] = [
-        "qrUrl": qrUrl,
-        "seminar": "part"
-    ]
-    request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
-    
-    let task = URLSession.shared.dataTask(with: request) { data, _, error in
-        guard let data = data, error == nil else {
-            print("🚨 Error: \(error?.localizedDescription ?? "Unknown error")")
+extension ReaderViewController {
+    func getValidQR(with qrUrl: String) {
+        print("❤️ \(qrUrl)")
+        guard let url = URL(string: "\(url)/validQR") else {
+            print("🚨 Invalid URL")
             return
         }
-
-        if let responseString = String(data: data, encoding: .utf8) {
-            print("✅ success: \(responseString)")
-        } else {
-            print("🚨 Error: Unable to convert data to string")
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let body: [String: AnyHashable] = [
+            "qrUrl": qrUrl,
+            "seminar": "part"
+        ]
+        request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                print("🚨 Error: \(error?.localizedDescription ?? "Unknown error")")
+                return
+            }
+            
+            if let responseString = String(data: data, encoding: .utf8) {
+                print("✅ success: \(responseString)")
+                if responseString.contains("잘못된") {
+                    DispatchQueue.main.async {
+                        ModalBuilder()
+                            .add(title: "출석 체크")
+                            .add(content: "유효하지 않은 QR 코드입니다.\n다시 시도해주세요.")
+                            .add(button: .confirm(title: "확인", action: {
+                                // 버튼 클릭 시의 동작
+                            }))
+                            .show(on: self)
+                        print("잘못된 큐알임임임임")
+                    }
+                }
+            } else {
+                print("🚨 Error: Unable to convert data to string")
+            }
         }
+        task.resume()
     }
-    task.resume()
 }
--- PARD/Sources/Qr/ReaderViewController.swift
+++ PARD/Sources/Qr/ReaderViewController.swift
@@ -88,13 +88,26 @@ extension ReaderViewController: ReaderViewDelegate {
         var message = ""
         switch status {
         case let .success(code):
+//            getValidQR(with: code)
+//            ModalBuilder()
+//                .add(title: "출석 체크")
+//                .add(image: "alreadyAttendance")
+//                .add(button: .confirm(title: "확인", action: {
+//                }))
+//                .show(on: self)
             guard let code = code else {
-                title = "에러"
-                message = "QR코드 or 바코드를 인식하지 못했습니다.\n다시 시도해주세요."
+//                ModalBuilder()
+//                    .add(title: "출석 체크")
+//                    .add(content: "유효하지 않은 QR 코드입니다.\n다시 시도해주세요.")
+//                    .add(button: .confirm(title: "확인", action: {
+//                    }))
+//                    .show(on: self)
+//                title = "에러"
+//                message = "QR코드 or 바코드를 인식하지 못했습니다.\n다시 시도해주세요."
                 break
             }
-            title = "알림"
-            message = "인식성공\n\(code)"
+//            title = "알림"
+//            message = "인식성공\n\(code)"
             getValidQR(with: code)
         case .fail:
             title = "에러"
@@ -110,11 +123,11 @@ extension ReaderViewController: ReaderViewDelegate {
             }
         }
         
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
-        
-        alert.addAction(okAction)
-        self.present(alert, animated: true, completion: nil)
+//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//        
+//        let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+//        
+//        alert.addAction(okAction)
+//        self.present(alert, animated: true, completion: nil)
     }
 }
