07d7206cf202531e74b835bb9f4dd0014d4bec44
jinsejin <j03298969@gmail.com>
2024-07-28 23:48:00 +0900

    [fix] 내점수 페이지 점수기록 컬렉션뷰가 데이터에 들어옴에 따라 뷰를 다시 그려주는 기능 추가

--- PARD/Sources/PardAppModel.swift
+++ PARD/Sources/PardAppModel.swift
@@ -90,13 +90,57 @@ class ReasonManager {
     static let shared = ReasonManager()
     private init() {}
     var reasonList: [ReasonPardnerShip] = []
-    
-    func fetchReasons(completion: @escaping ([ReasonPardnerShip]) -> Void) {
-        getReason { reasons in
-            self.reasonList = reasons
-            completion(reasons)
+    var reason: ReasonPardnerShip?
+
+    func getReason(completion: @escaping ([ReasonPardnerShip]) -> Void) {
+        guard let urlLink = URL(string: url + "/reason") else {
+            print("🚨 잘못된 URL")
+            return
         }
+        
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: urlLink) { data, response, error in
+            if let error = error {
+                print("🚨 에러:", error)
+                return
+            }
+            
+            guard let JSONdata = data, !JSONdata.isEmpty else {
+                print("🚨 [getReason] 에러: 데이터가 없거나 빈 데이터")
+                return
+            }
+            
+            if let dataString = String(data: JSONdata, encoding: .utf8) {
+                print("🌱 응답 데이터 문자열: \(dataString)")
+            } else {
+                print("🚨 데이터를 문자열로 변환할 수 없음")
+            }
+            
+            let decoder = JSONDecoder()
+            do {
+                if let reason = try? decoder.decode(ReasonPardnerShip.self, from: JSONdata) {
+                    print("✅ 성공: Reason")
+                    DispatchQueue.main.async {
+                        self.reason = reason
+                        completion([reason])
+                    }
+                } else {
+                    let reasonArray = try decoder.decode([ReasonPardnerShip].self, from: JSONdata)
+                    print("✅ 성공: Reason")
+                    DispatchQueue.main.async {
+                        self.reasonList = reasonArray
+                        completion(reasonArray)
+                    }
+                }
+            } catch {
+                print("🚨 디코딩 에러:", error)
+            }
+        }
+        
+        task.resume()
     }
+
+
 }
 
 // MARK: - total rank
