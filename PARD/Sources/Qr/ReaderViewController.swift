ed174608a4f90de2f707b680f2e92d77fcf4cdce
haram22 <kimhr9889@handong.ac.kr>
2024-08-12 14:30:54 +0900

    [fix] qr 배경색 투명하도록 조정

--- PARD/Sources/Qr/ReaderViewController.swift
+++ PARD/Sources/Qr/ReaderViewController.swift
@@ -17,10 +17,21 @@ class ReaderViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 133/255.0, green: 133/255.0, blue: 133/255.0, alpha: 0.6)
-        setAttribute()
-        setupLayouts()
-        readerView.start()
+//        view.backgroundColor = .clear
+//        setAttribute()
+//        setupLayouts()
+//        readerView.start()
+        view.backgroundColor = .clear
+
+            // 카메라 뷰를 먼저 추가
+            readerView = QrReaderView()
+            readerView.delegate = self
+            view.addSubview(readerView)
+            
+            // 다른 UI 요소 추가
+            setAttribute()
+            setupLayouts()
+            readerView.start()
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -37,47 +48,89 @@ class ReaderViewController: UIViewController {
     }
     
     func setAttribute() {
+        // QR 리더 뷰 초기화 및 추가
         readerView = QrReaderView()
         readerView.delegate = self
         view.addSubview(readerView)
+
+        // 타이틀 라벨 설정 및 추가
         titleLabel.text = "테두리 안에 출석 QR코드를 인식해주세요."
         titleLabel.font = UIFont.pardFont.head2
         titleLabel.textColor = .pard.blackBackground
         view.addSubview(titleLabel)
-        outlineIcon.image = UIImage(named: "qr_outline")
+
+        // 이미지 설정 및 추가
+        outlineIcon.image = UIImage(named: "qr_background")
+        outlineIcon.contentMode = .scaleAspectFit
         view.addSubview(outlineIcon)
+
+        // 버튼 설정 및 추가
         cancelButton.setImage(UIImage(named: "cancelButton"), for: .normal)
         cancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
         view.addSubview(cancelButton)
     }
-    
+
     func setupLayouts() {
-        readerView.snp.makeConstraints{ make in
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview()
-            make.width.height.equalTo(280)
+        // readerView를 전체 화면으로 설정
+        readerView.snp.makeConstraints { make in
+            make.edges.equalToSuperview() // 전체 화면을 차지하도록 설정
         }
-        titleLabel.snp.makeConstraints{ make in
-            make.bottom.equalTo(readerView.snp.top).offset(-28)
-            make.centerX.equalToSuperview()
+
+        // 타이틀 라벨 레이아웃 설정
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(outlineIcon.snp.centerX)
+            make.top.equalTo(232)
         }
-        outlineIcon.snp.makeConstraints{ make in
-            make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview()
+
+        // 이미지 레이아웃 설정 (화면 중앙에 배치)
+        outlineIcon.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
-        cancelButton.snp.makeConstraints{ make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(readerView.snp.bottom).offset(80)
+
+        // 버튼을 이미지 위에 배치
+        cancelButton.snp.makeConstraints { make in
+            make.centerX.equalTo(outlineIcon.snp.centerX)
+            make.top.equalTo(626)
+            make.width.height.equalTo(50)
         }
+        
+        // zPosition은 위에 쌓을 때의 순서. 즉, outlineIcon위에 cancelButton, titleLabel이 있음
+        outlineIcon.layer.zPosition = 0
+        cancelButton.layer.zPosition = 1
+        titleLabel.layer.zPosition = 1
     }
+
+
+//    func setupLayouts() {
+//        //        readerView.snp.makeConstraints{ make in
+//        //            make.centerX.equalToSuperview()
+//        //            make.centerY.equalToSuperview()
+//        //            make.width.height.equalTo(280)
+//        //        }
+//        readerView.snp.makeConstraints { make in
+//            make.edges.equalToSuperview()
+//        }
+//        titleLabel.snp.makeConstraints { make in
+//            make.bottom.equalTo(readerView.snp.top).offset(-28)
+//            make.centerX.equalToSuperview()
+//        }
+//        outlineIcon.snp.makeConstraints{ make in
+//            make.centerX.equalToSuperview()
+//            make.centerY.equalToSuperview()
+//        }
+//        cancelButton.snp.makeConstraints{ make in
+//            make.centerX.equalToSuperview()
+//            make.top.equalTo(readerView.snp.bottom).offset(80)
+//        }
+//    }
     
     @objc func cancelButtonTapped() {
         print("button Tapped")
         
         self.navigationController?.popViewController(animated: true)
-//        if let tabBarController = self.tabBarController {
-//            tabBarController.selectedIndex = 0
-//        }
+        //        if let tabBarController = self.tabBarController {
+        //            tabBarController.selectedIndex = 0
+        //        }
     }
 }
 
@@ -88,13 +141,13 @@ extension ReaderViewController: ReaderViewDelegate {
         var message = ""
         switch status {
         case let .success(code):
-//            getValidQR(with: code)
-//            ModalBuilder()
-//                .add(title: "출석 체크")
-//                .add(image: "alreadyAttendance")
-//                .add(button: .confirm(title: "확인", action: {
-//                }))
-//                .show(on: self)
+            //            getValidQR(with: code)
+            //            ModalBuilder()
+            //                .add(title: "출석 체크")
+            //                .add(image: "alreadyAttendance")
+            //                .add(button: .confirm(title: "확인", action: {
+            //                }))
+            //                .show(on: self)
             guard let code = code else {
                 break
             }
